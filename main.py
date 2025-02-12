62e0f86fad3589636040f5ddc3b97c2f2643c50f
aakashsell <aakashsell@gmail.com>
2025-02-12 13:00:05 -0500

    basic outline of api

--- main.py
+++ main.py
@@ -11,21 +11,44 @@ sessions['last_id'] = 0
 def create_session():
     session_id = sessions['last_id'] + 1
     sessions['last_id'] = session_id
+    sessions[session_id]['global'] = {}
+    sessions[session_id]['local'] = {}
+
     return jsonify({"session_id": session_id})
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    data = request.get_json()
+@app.route('/run', methods=['POST'])
+def run_code():
+    data = request.get_json() 
+
     if not data:
         return jsonify({'error': 'No JSON data received'}), 400
     
-    return jsonify({'message': 'Data received', 'data': data}), 200
+    if data.get('session_id', 0) == 0:
+        return jsonify({'message': 'no session id recieved'}), 400
+    
+    session_id = data['session_id']
 
+    if sessions.get(session_id, 0) == 0:
+        return jsonify({'message': 'invalid session id'}), 400
+    
+    session = sessions.get(session_id)
 
-@app.route('/test', methods=['POST'])
-def test():
-    print(eval("a + 1"))
-    return jsonify({"value": eval("a + 1")})
+    global_vars = session['global']
+    local_vars = session['local']
+
+    if data.get('user_input', 0) == 0:
+        return jsonify({'message': 'no code sent'}), 400
+    
+    code = data.get('user_input')
+
+    try:
+        output = eval(code, global_vars, local_vars)
+        return jsonify({'code_output': output}), 200
+    except Exception:
+        print("error")
+    
+        return jsonify({'message': 'Data received', 'data': data}), 200
+
 
 
 if __name__ == '__main__':
